4ed4c300f8d5b0b97006b81c0bdecbd0cacb6115
José Pablo Medina Grande <jose.pablo.medina.grande@everis.com>
2021-01-04 19:34:42 +0100

    Using PostDto

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Infrastructure.Repositories;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -21,19 +23,42 @@ namespace SocialMedia.Api.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+            var postDto = posts.Select(x => new PostDto
+            {
+                 UserId = x.PostId,
+                 PostId = x.PostId,
+                 Description = x.Description,
+                 Image = x.Image,
+                 Date = x.Date
+            });
+            return Ok(postDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+            var postDto = new PostDto
+            {
+                UserId = post.PostId,
+                PostId = post.PostId,
+                Description = post.Description,
+                Image = post.Image,
+                Date = post.Date
+            };
+            return Ok(postDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            var post = new Post
+            {
+                UserId = postDto.PostId,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                Date = postDto.Date
+            };
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
